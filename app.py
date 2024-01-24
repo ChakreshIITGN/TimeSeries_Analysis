c94ae3a06f9337814d58d8ff6fcf8c0dee338bf7
Chakresh Kr. Singh <34956550+ChakreshIITGN@users.noreply.github.com>
2024-01-25 00:17:04 +0100

    adding demo notebook showcasing all the procedures and steps of analysis

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+import numpy as np 
+import statsmodels as sm 
+
+
+st.title('Time Series Analysis')
+
+
+st.sidebar('Chose your defaults')
---
+++ test.py
@@ -0,0 +1,32 @@
+### Tests for checking and evaluating the functionality of the codes
+
+def test_checkData():
+    # checks if the data loaded satisfies the constraints 
+    # that is the minimum date and maximum date and total length
+
+    return None 
+
+def test_tsDecompose():
+    # checks the time series decomposition using statsmodels    
+    return None
+
+def test_tsSmoothing():
+    # checks the smoothing functions - aka exponential smoothing
+    return None 
+
+def test_tsCorr():
+    # checks if the Auto-Correlation (ACF) and the Partial Auto-Correlation (PACF) function work well 
+    return None 
+
+def test_tsPredict_ARMA():
+    # checks the functioning of ARMA implementation
+    return None 
+
+def test_tsPredict_Prophet():
+    # check the implementation of the Meta Prophet implementation
+    return None
+
+def test_tsPredcit_RNN():
+    # checks the implementation of the RNNS networks for prediction analysis
+    return None 
+
