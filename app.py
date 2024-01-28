f08a271668bd4336d8ecd73923fa3340a8148bcd
Chakresh Kr. Singh <34956550+ChakreshIITGN@users.noreply.github.com>
2024-01-28 11:28:26 +0100

    Added the tabs for easy navigation, added forecasting using Prophet

--- app.py
+++ app.py
@@ -1,9 +1,249 @@
+from helper_functions import *
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf, month_plot
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima.model import ARIMA as arima_model
+from statsmodels.tsa.stattools import kpss
+
+
+import time
 import streamlit as st 
 import numpy as np 
 import statsmodels as sm 
+import pandas as pd 
+import datetime
+
+######### initial setup 
+
+companyNames = ['AMAZON', 'APPLE', 'GOOGLE', 'META', 'NETFLIX']
+st.set_page_config(page_title='TSA@streamlit', layout='wide')
+st.title(':orange[Time Series Analysis]')
 
 
-st.title('Time Series Analysis')
+######### About the app ###############
+markdown_about_msg = """
+        ##### The app implements the basics of time series analysis.
+
+        Data source :  KAGGLE : [link](https://www.kaggle.com/datasets/nikhil1e9/netflix-stock-price) to the data set 
+
+        :blue[KeyWords] :  **Time Series Analysis, ARMA, SeriesDecomposition, RNN, LSTM, Forecasting**
+
+        Companies used in the dataset MAANG = Meta, Apple, Amazon, Netflix and, Google
+    """
 
 
-st.sidebar('Chose your defaults')
+
+############ SIDEBAR for setting the parameters ##########################
+with st.sidebar:
+    st.header(':red[Chose your Parameters]')
+    st.write(" :violet[Running on Streamlit version] -- " + st.__version__)
+
+
+    dates = st.date_input(label=':orange[Enter the date range for the analysis]',
+                          value = (datetime.date(2019,1,1), datetime.date(2024,1,10)),
+                          min_value=datetime.date(2018,1,1), 
+                          max_value=datetime.date(2024,1,10),
+                          format="YYYY-MM-DD")
+    
+    minDate,maxDate = str(dates[0]), str(dates[1])
+    logData = st.radio(':orange[Logged Values]', options = [True, False],index=None)
+    company = st.radio(':orange[Chose the company to analyse]', options=companyNames)
+    window_size = st.slider(':orange[Chose the rolling window size]',min_value=5,max_value=50,value=28)
+    monthly_plot = st.button('Show Monthly Plot')
+
+    ts_decompose_model = st.radio('# :orange[Choose Decomposition Model]', options=['additive', 'multiplicative'])
+
+    split_at = st.number_input(':orange[Split data into Train-Test starting from the end]', 
+                               min_value=100,max_value=500,value=250,step=50)
+    
+    ## ARIMA model parameters
+    st.subheader(':green[ARIMA model parameters]')
+    p = st.number_input(':green[p]', min_value=0,max_value=30,value=1)
+    d = st.number_input(':green[d]', min_value=0,max_value=30,value=1)
+    q = st.number_input(':green[q]', min_value=0,max_value=30,value=0)
+    
+
+
+tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["About", "Exploration", "ARIMA models",
+                                              "Prophet", "DeepLearning Models", "Load Your Data" , "Next Steps"])
+
+
+with tab1:
+
+    st.markdown(markdown_about_msg)
+
+    def load_data(logData):
+        df = create_AnalysisData(companyNames,minDate,maxDate,logData)
+        return df
+
+    DF = load_data(logData)
+
+    col1,col2 = st.columns(2,gap="medium")
+
+    with col1:
+        st.subheader(':orange[Data frame head]')
+        st.dataframe(DF.head(7))
+
+    with col2:
+
+        st.subheader(f':orange[Time Series View]',)
+        st.line_chart(DF)
+
+    with st.expander(':orange[Expand to see the summary statistics]'):
+
+        st.write((DF.describe()))
+
+
+    st.divider()
+
+    ##### End Message
+    st.markdown(':orange[:heart: and 🕊️ for all - Chakresh]')
+
+with tab2:
+
+    st.subheader(f'Analysis for :orange[{company}] time series Data: ')
+
+    timeSeriesData = DF[company]
+    timeSeriesData_rolling = timeSeriesData.rolling(window_size).mean().dropna()
+    st.line_chart(timeSeriesData_rolling)
+
+
+    if monthly_plot:
+        st.subheader(f'Monthly price plot for :orange[{company}] data')
+        show_monthly_sale(timeSeriesData)
+
+    st.divider()
+
+
+    st.subheader('Time Series Decomposition')
+
+    st.write(f'with decomposition model as :blue[{ts_decompose_model}]')
+
+    ts_decomposition = seasonal_decompose(x=timeSeriesData[:-1],model=ts_decompose_model,period=30)
+
+    T,S,R = ts_decomposition.trend, ts_decomposition.seasonal, ts_decomposition.resid
+
+    with st.expander("See the Trend, Seasonality and Residual Plots"):
+
+        st.subheader('Trend')
+        st.line_chart(T)
+        st.subheader('Seasonality')
+        st.line_chart(S)
+        st.subheader('Residual')
+        st.line_chart(R,width=1)
+
+
+    # st.header()
+        
+    st.subheader('Exponential Smoothing - (Done on the rolling average series)')
+    st.markdown("""
+            Single Exponential Smoothing 
+                
+            Double Exponential Smoothing 
+                
+            Triple Exploential Smoothing
+    """)
+
+
+    smoothing_type = st.multiselect(':orange[Chose Smoothing Type]',options=['Single', 'Double', 'Triple'], default=['Single'])
+
+    # Creating the training and the test data. We do it outside the functions for increased scope. 
+    training_Data,test_Data = timeSeriesData_rolling[:-split_at], timeSeriesData_rolling[-split_at:]
+
+
+
+    gen_smooth = st.button('Generate Smoothing ')
+
+    if gen_smooth:
+        generate_smoothing(smoothing_type,ts_decompose_model,training_Data,test_Data,timeSeriesData_rolling,split_at)
+
+    st.divider()
+
+
+with tab3:
+
+    st.subheader('Time Series Models - ARIMA (p,d,q)')
+
+    st.markdown(""" ##### Before we fit ARIMA models we run stationarity tests on the time series""")
+
+    with st.expander('Stationarity Test (KPSS) Results'):
+
+        check_stationarity(training_Data)
+
+    arima_model = arima_model(training_Data,order=(p,d,q))
+    arima_resid = arima_model.fit()
+
+
+
+    with st.expander('Model fit summary and diagnostics plots for arima model'):
+
+        st.write(arima_resid.summary())
+        fig3 = plt.figure()
+        ax = fig3.add_subplot(111)
+
+
+        st.pyplot(arima_resid.plot_diagnostics(figsize=(12,10)))
+
+
+    arima_forecast = st.button('Get ARIMA forecast')
+
+    if arima_forecast:
+        arima_preds,conf_90,conf_95 = get_arima_forecast(arima_resid,split_at)
+        train_time,test_time = timeSeriesData_rolling.index.values[:-split_at], timeSeriesData_rolling.index.values[-split_at:]
+        plot_arima_forecast(arima_preds,conf_90,conf_95,p,d,q,company,training_Data,test_Data,train_time,test_time)
+
+
+    st.divider()
+
+with tab4:
+
+    st.subheader('Facebook - Prophet')
+
+    prophet_prediction(timeSeriesData_rolling,company)
+
+    st.divider()
+
+
+with tab5:
+
+    st.header('Deep Learning')
+
+    st.divider()
+
+
+with tab6:
+
+    message = """
+                This is a work in progress. Soon you will be able to analyse your own time series data
+
+                I appreciate your patience - 🙏
+
+                """
+
+    st.markdown(message)
+    
+    upload_file  = st.file_uploader("load your data",)
+
+    # with st.sidebar():
+
+    #     st.write("tune parameters for your own data")
+
+with tab7:
+    
+    next_steps = """
+
+        ### :red[what to expect in future versions]
+
+        1. `User defined` arcitecture for the RNN and LSTM models
+        
+        2. `SARIMA models`
+        
+        3. `Theoretical` explanantion for the parameters search in statistical models
+        
+        4. `Scraping` data from online 
+
+        5. Implementation of `quantum algorithms` in forecasting.  
+    """
+
+    st.markdown(next_steps)
+
+    st.divider()
---
+++ helper_functions.py
@@ -0,0 +1,316 @@
+from statsmodels.tsa.stattools import kpss
+from statsmodels.tsa.api import SimpleExpSmoothing, Holt, ExponentialSmoothing
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf, month_plot
+from prophet import Prophet
+
+import matplotlib.pyplot as plt
+
+import statsmodels as sm 
+import pandas as pd 
+import datetime
+import numpy as np
+import pandas as pd
+import streamlit as st
+import time
+
+
+
+### Function to standardise the data
+
+def standardiseData(dataFrame,minDate,maxDate):
+    """
+        Restircts the data betweeen the chosen dates for uniformity. 
+        Then Resamples using freq='B' i.s considering only the business days
+
+        All the missing values are filled using 'bfill' 
+        ============================================
+        arguments : 
+
+        minDate: minimum date for the analysis
+        maxDate: maximum date for the analysis
+
+    """
+    new_index = pd.date_range(minDate,maxDate,freq='B') # dates between which we obderve the time Series
+
+    dataFrame['Date'] = pd.to_datetime(dataFrame['Date'],yearfirst=True)
+    dataFrame.set_index('Date',inplace=True)
+    dataFrame = dataFrame[minDate:maxDate]
+
+    newDF = dataFrame.reindex(new_index,method='bfill')
+
+    newDF = newDF[['Close']]
+    return newDF
+
+## test function to check the data 
+
+def checkData(df,minDate,maxDate):
+    """
+        Sanity check on the dataframe before any analysis
+
+        checks for  - length, datatypes
+
+        ==============================
+        arguments: 
+
+        df: dataframe to be checked
+        minDate: minimum date for the analysis
+        maxDate: maximum date for the analysis
+
+    """
+    days = len(pd.date_range(minDate,maxDate,freq='B'))
+
+    assert len(df) == days
+    assert df['Close'].dtypes == 'float64'
+    assert df.index.dtype == '<M8[ns]'
+
+    return None
+
+
+### Creating the Data for analysis combining from different 
+### .csv files 
+def create_AnalysisData(companyNames,minDate,maxDate,logData=False):
+
+    """
+        takes the company names in the existing data folder 
+        Creates the standardised data set for the analysis
+
+        =================================================
+        arguments:
+        
+        if logData == True : converts the Numeric data to log10
+
+        minDate: minimum date for the analysis
+        maxDate: maximum date for the analysis
+    """
+
+    listDF = []
+    final_cols = []
+    for i in range(len(companyNames)):
+        final_cols.append(companyNames[i])
+        df_i = pd.read_csv(f'data/{companyNames[i]}_daily.csv', header=0)
+        df_i = standardiseData(df_i,minDate,maxDate)
+        checkData(df_i,minDate,maxDate)
+        
+        listDF.append(df_i)
+
+    finalDF = pd.concat(listDF,ignore_index=False,axis=1)
+    finalDF.columns = final_cols
+
+    if logData:
+
+        finalDF = finalDF.apply(np.log10,axis=0)
+
+    return finalDF
+
+def smoothing(smoothing_type,ts_decompose_model, train, test):
+    """
+        This function creates the smoothing for the time series based on
+        the user selected options. 
+
+        =================================================
+        arguments:
+
+        smoothing_type     : list of smoothing types 'Single', 'Double', 'Triple' 
+        ts_decompose_model : str, defines the decomposition b/w additive or multiplicative
+        train              : training data
+        test               : test data
+        returns - list of predictions for each chosen smoothing   
+    """
+    predictions = []
+
+    if 'Single' in smoothing_type:
+
+        ## Single Exponential Smoothing 
+        singleExp = SimpleExpSmoothing(train).fit(optimized=True)
+        singleExp_pred = singleExp.forecast(len(test))
+        predictions.append(singleExp_pred)
+    
+    if 'Double' in smoothing_type:
+
+        # Double Exponential Smoothing 
+        doubleExp = Holt(train).fit(optimized=True)
+        doubleExp_pred = doubleExp.forecast(len(test))
+        predictions.append(doubleExp_pred)
+
+    if 'Triple' in smoothing_type:
+
+        # Triple Exponential Smoothing
+        tripleExp = ExponentialSmoothing(train,
+                                        trend=ts_decompose_model,
+                                        seasonal=ts_decompose_model,
+                                        seasonal_periods=100).fit(optimized=True)
+        tripleExp_pred = tripleExp.forecast(len(test))
+        predictions.append(tripleExp_pred)
+    
+
+    return predictions
+
+
+def plot_smoothing(predictions,timeSeriesData_rolling,split_at,train,test):
+
+    """
+        Plots the smoothing for the selected types as a matplotlib figure
+    """
+    
+    train_time,test_time = timeSeriesData_rolling.index.values[:-split_at], timeSeriesData_rolling.index.values[-split_at:]
+
+    fig2 = plt.figure(figsize=(12,4))
+    ax = fig2.add_subplot(111)
+
+    ax.set_title(f'Exponential smoothing on time series')
+
+    ax.plot(train_time,train,'--b',label='Train')
+    ax.plot(test_time,test,'--', color='gray',label='Test')
+
+    for i,val in enumerate(predictions):
+
+        if i == 0:
+            label='Single Exp. Predictions'
+            color='red'
+        if i == 1:
+            label='Double Exp. Predictions'
+            color='green'
+        if i == 2:
+            label='Triple Exp. Predictions'
+            color='darkorange'
+
+        ax.plot(test_time,val,'--', color=color,label=label)
+        
+    ax.legend()
+    st.pyplot(fig2)
+    return None
+
+def generate_smoothing(smoothing_type,ts_decompose_model,train,test,timeSeriesData_rolling,split_at):
+    
+    with st.spinner('Fitting Exponential'):
+        time.sleep(3)
+        predictions = smoothing(smoothing_type,ts_decompose_model,train,test)
+        plot_smoothing(predictions,timeSeriesData_rolling,split_at,train,test)
+        
+    st.success('Done')
+    return None
+
+def show_monthly_sale(timeSeriesData):
+
+    timeSeriesData_monthly = timeSeriesData.resample('M').sum()
+    fig1 = plt.figure(figsize=(12,4))
+    ax = fig1.add_subplot(111)
+    _ = month_plot(timeSeriesData_monthly[:-1],ax=ax)
+    st.pyplot(fig1)
+
+    return None 
+
+def check_stationarity(train,diff=0):
+
+    statistic, p_value, n_lags, critical_values = kpss(train)
+
+    stationarity_message = f"""
+
+    :red[DIFF = {diff}]
+
+    :blue[KPSS Statistic] : {np.round(statistic,4)}, 
+
+    :blue[p-value]        : {p_value}, 
+    
+    :blue[num lage]       : {n_lags}, 
+
+
+    :blue[Critical Values] - 
+    {critical_values}
+
+    :orange[RESULT : The series is {"not " if p_value < 0.05 else ""}stationary]
+
+    
+
+                         """
+
+    st.write(stationarity_message)
+    
+    if p_value < 0.05:
+        diff += 1
+        train = train.diff().dropna()
+        check_stationarity(train,diff=diff)
+
+
+    return None
+
+def get_arima_forecast(arima_resid,split_at):
+
+    arima_pred = arima_resid.forecast(split_at,alpha=0.05)
+    fitted = arima_resid.get_forecast(split_at)
+
+    conf_int_95 = fitted.conf_int(alpha=0.05)
+    conf_int_90 = fitted.conf_int(alpha=0.10)
+
+    return arima_pred,conf_int_90,conf_int_95
+
+def plot_arima_forecast(pred,conf_90,conf_95,p,d,q,company,train,test,train_time,test_time):
+
+    arima_pred = pred
+    conf_int_90 = conf_90
+    conf_int_95 = conf_95
+
+    fig4 = plt.figure(figsize=(12,6))
+    ax = fig4.add_subplot(111)
+
+    ax.set_title(f'ARIMA {p,d,q} model predictions for {company}')
+
+    ax.plot(train_time,train,'--b',label='Train')
+    # ax.plot(train_time[1:],arima_resid.fittedvalues[1:],'-k',label='Model Fitted Values')
+    ax.plot(test_time,test,'--', color='gray',label='Test')
+    ax.plot(test_time,arima_pred,'--', color='red',label='ARMA predictions')
+    ax.fill_between(
+        conf_int_95.index,
+        conf_int_95[f"lower {company}"],
+        conf_int_95[f"upper {company}"],
+        color="b",
+        alpha=0.1,
+        label="95% CI"
+    )
+
+    ax.fill_between(
+        conf_int_90.index,
+        conf_int_90[f"lower {company}"],
+        conf_int_90[f"upper {company}"],
+        color="b",
+        alpha=0.2,
+        label="90% CI"
+    )
+    ax.legend(loc="upper left")
+
+    st.pyplot(fig4)
+
+    return None
+
+
+def prophet_prediction (train,company):
+
+    ##### put a test here to see that the train data is the data for only 1 company and has an index and a value column
+
+    prophet_model = Prophet()
+
+    df_modify = pd.DataFrame()
+    df_modify['ds'] = train.index
+    df_modify['y'] = train.values
+
+    prophet_model.fit(df_modify)
+
+    # preparing prediction dataframe 
+
+    future = prophet_model.make_future_dataframe(periods = 100,freq = 'B') 
+
+    prophet_forecast = prophet_model.predict(future)
+
+    st.write(prophet_forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+    
+    fig5 = plt.figure(figsize=(12,4))
+    ax = plt.subplot(111)
+    _ = prophet_model.plot(prophet_forecast,ax=ax,xlabel='Days',ylabel='Closing value',include_legend=True)
+    # ax.plot(test,'r--', label='Observed Data')
+    ax.set_title(f'{company} stock forecast using Facebook Prophet')
+    ax.legend()
+
+    st.pyplot(fig5)
+
+    return None
+
